8bb6cb6cad98f95bab7cf2fada11fe0c99146fb6
github username <github email address>
2018-03-23 16:12:27 -0400

    added login errors1

--- src/url.js
+++ src/url.js
@@ -5,8 +5,8 @@ let api;
 //   api = "https://blackjack-backend.herokuapp.com/"
 // }
 
-  api = "http://localhost:3000/"
-// api = "https://blackjack-backend.herokuapp.com/"
+  // api = "http://localhost:3000/"
+api = "https://blackjack-backend.herokuapp.com/"
 
 
 export default api
