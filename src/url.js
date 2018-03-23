7b7456bc6b86ae0e4777e869bdf83d2b23b4378e
github username <github email address>
2018-03-23 15:40:01 -0400

    fixing heroku1

--- src/url.js
+++ src/url.js
@@ -1,8 +1,12 @@
 let api;
-if (process.env.NODE_ENV === "development") {
-  api = "http://localhost:3000/"
-} else {
-  api = "https://blackjack-backend.herokuapp.com/"
-}
+// if (process.env.NODE_ENV === "development") {
+//   api = "http://localhost:3000/"
+// } else {
+//   api = "https://blackjack-backend.herokuapp.com/"
+// }
+
+//   api = "http://localhost:3000/"
+api = "https://blackjack-backend.herokuapp.com/"
+
 
 export default api
